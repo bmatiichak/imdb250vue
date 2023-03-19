3bd9e7d61fd1b6e4e26a421724326c6b6b3ac5c6
Bohdan Matiichak <bmatiychak@gmail.com>
2023-03-20 00:59:42 +0200

    movies store was set up

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { createStore } from "vuex";
+import moviesStore from "./modules/movies";
 
 export default createStore({
   state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    moviesStore,
+  },
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Some Header</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mapActions } from "vuex";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  mounted() {
+    this.fetchMovies();
+  },
+  methods: {
+    ...mapActions("moviesStore", ["fetchMovies"]),
   },
 };
 </script>
